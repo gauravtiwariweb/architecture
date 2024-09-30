d6357d7db4924f0ada18b78c739e49b2f3c561d3
Kanak Gupta <129761215+kanak-gupta30@users.noreply.github.com>
2024-09-30 16:19:15 +0530

    script.js

--- script.js
+++ script.js
@@ -9,24 +9,3 @@ leftButton.addEventListener('click', () => {
 rightButton.addEventListener('click', () => {
     carouselInner.scrollBy({ left: 300, behavior: 'smooth' });
 });
-// const prevButton = document.getElementById('prev-button');
-// const nextButton = document.getElementById('next-button');
-// const profileCards = document.querySelector('.profile-cards');
-
-// let scrollAmount = 0;
-
-// prevButton.addEventListener('click', () => {
-//     profileCards.scrollBy({
-//         top: 0,
-//         left: -220,
-//         behavior: 'smooth'
-//     });
-// });
-
-// nextButton.addEventListener('click', () => {
-//     profileCards.scrollBy({
-//         top: 0,
-//         left: 220,
-//         behavior: 'smooth'
-//     });
-// });
